d52208561b00eddfb2033af0ae665c636012a792
whisillus <whisillus@gmail.com>
2026-03-09 11:34:06 +0800

    update

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -21,7 +21,7 @@ local snacks = {
     scroll = { enabled = true },
     statuscolumn = { enabled = true },
     terminal = { enabled = true },
-    words = { enabled = true },
+    -- words = { enabled = true },
     styles = {
       notification = {
         -- wo = { wrap = true } -- Wrap notifications
@@ -106,8 +106,8 @@ local snacks = {
     { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
     { "<c-/>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
     -- { "<c-_>",      function() Snacks.terminal() end, desc = "which_key_ignore" },
-    { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
-    { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
+    -- { "]]",         function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
+    -- { "[[",         function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
     {
       "<leader>N",
       desc = "Neovim News",
