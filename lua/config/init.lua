c4c9d3a4f834255d0457f1b6fc7327930d7cc17b
whisillus <whisillus@gmail.com>
2026-03-09 13:18:24 +0800

    update lsp

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -2,3 +2,5 @@
 require("config.config")
 require("config.keymap")
 require("config.lazy")
+
+require("lsp.init")
---
+++ lua/lsp/init.lua
@@ -0,0 +1,4 @@
+
+require("lsp.clangd")
+require("lsp.lua-ls")
+require("lsp.pyright")
